2635b25f7ecd1f756b5c0220a97bd4e5d60eefa2
Amber Crawford <ammcrawford@gmail.com>
2015-09-28 10:53:19 -0600

    Updated word finder

--- lib/word_finder.rb
+++ lib/word_finder.rb
@@ -1,6 +1,17 @@
+require 'pry'
+
 class WordFinder
   def words
     File.read('/usr/share/dict/words').split("\n")
   end
-  
+
+  def for_letters(letters)
+    word_options = words.select{|word|
+      word.chars.each do |letter|
+        if letters.include?(letter.downcase)
+        end
+      end
+    }
+  end
+
 end
--- test/word_finder_test.rb
+++ test/word_finder_test.rb
@@ -5,5 +5,25 @@ require 'minitest/pride'
 require 'pry'
 
 class WordFinderTest < Minitest::Test
+  def test_it_returns_word_options_for_letter_set
+    finder = WordFinder.new
+    assert finder.for_letters(['r', 'u', 'n']).include?("run")
+  end
+
+  def test_it_returns_multiple_word_options_for_letter_set
+    finder = WordFinder.new
+    assert finder.for_letters(['r', 'u', 'n']).count > 1
+  end
+
+  def test_it_returns_word_with_letters_out_of_order
+    finder = WordFinder.new
+    assert finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o']).include?('aer')
+  end
+
+  def test_it_returns_multiple_words
+    finder = WordFinder.new
+    assert finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o']).include?('aer')
+    assert finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o']).include?('toe')
+  end
 
 end
